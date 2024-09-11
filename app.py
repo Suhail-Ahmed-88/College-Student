8fa62fb022dace89619f8245ffd12ac420f7e0c1
Suhail-Ahmed-88 <erajpar.suhail.ahmed@gmail.com>
2024-09-11 20:41:42 +0800

    Data Ingestion

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from src.Student_Project.logger import logging
 from src.Student_Project.exeception import CustomException
+from src.Student_Project.components.data_ingestion import DataIngestion
+from src.Student_Project.components.data_ingestion import DataIngestionConfig 
 import sys
+import os
+import pymysql
 
 if __name__ == "__main__":
     logging.info("The Execution has Started")
@@ -8,7 +12,9 @@ if __name__ == "__main__":
     
     
 try:
-    a=1/0
+    # deta_ingestion_config=DataIngestionConfig()
+    data_ingestion=DataIngestion()
+    data_ingestion.initiate_data_ingestion()
 except Exception as e:
     logging.info("Custom Exception")
     raise CustomException(e,sys)   
